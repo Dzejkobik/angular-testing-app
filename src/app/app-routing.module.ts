ea09619c46449a7d3386d83e5fec6c0a2b3dfc59
Dzejkobik <39236037+Dzejkobik@users.noreply.github.com>
2019-07-26 21:21:32 +0200

    Project with authentication, interceptors, routing, guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { HomeGuard } from './guards/home.guard';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login',
+   component: LoginComponent,
+   canActivate: [AuthGuard]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [HomeGuard]
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [HomeGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators'
+import { Tokens } from '../models/Tokens';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  private loggedUser: string;
+
+   config : {
+    apiUrl: ""
+  }
+
+  constructor(private http: HttpClient) { }
+
+  login(user: {username: string, password: string}) : Observable<boolean> {
+    return this.http.post<any>(`${this.config.apiUrl}/login`, user)
+      .pipe(
+        tap(tokens => this.doLoginUser(user.username, tokens)),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        })
+      )
+  }
+
+  private doLoginUser(username: string, tokens: Tokens) {
+    this.loggedUser = username;
+    this.storeTokens(tokens);
+  }
+
+  private storeTokens(tokens: Tokens) {
+    localStorage.setItem(this.JWT_TOKEN, tokens.jwt);
+    localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
+  }
+
+  logout() {
+    return this.http.post<any>(`${this.config.apiUrl}/logout`, {
+      'refreshToken' : this.getRefreshToken()
+    }).pipe(
+      tap(() => this.doLogoutUser()),
+      mapTo(true),
+      catchError(error => {
+        alert(error.error);
+        return of(false);
+      })
+    )
+  }
+
+  private doLogoutUser() {
+    this.loggedUser = null;
+    this.removeTokens();
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
+
+  refreshToken() {
+    return this.http.post<any>(`${this.config.apiUrl}/refresh`, {
+      'refreshToken' : this.getRefreshToken()
+    }).pipe(tap((tokens: Tokens) => {
+      this.storeJwtToken(tokens.jwt);
+    }))
+  }
+
+  private getRefreshToken() {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  getJwtToken() {
+    return localStorage.getItem(this.JWT_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
+
+  isLoggedIn() {
+    return !!this.getJwtToken();
+  }
+}
